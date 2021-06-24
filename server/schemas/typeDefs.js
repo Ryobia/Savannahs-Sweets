c9984b1c1f58f727d69ba96418ed1ce0e54d9def
Clint Hansen <clint.hansen2009@gmail.com>
2021-06-23 18:21:28 -0600

    typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,28 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+}
 
+type Order {
+    _id: ID
+    orderDate: String
+    qauntity: Int
+    products: [Product]
+}
+
+type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    price: Float
+}
 `;
 
 module.exports = typeDefs;
