2200e8d0177d7746baf9be2bbc6002056571d997
Ryobia <ryobia36@gmail.com>
2021-06-29 19:49:31 -0600

    auth loggedin and logout stuff

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,21 @@
 import React from "react";
-
+import Auth from '../../utils/auth';
 import Logo from "../Logo";
 
 const Nav = (props) => {
   const { setCurrentPage } = props;
 
-
+if (Auth.loggedIn()) {
   return (
     <div className="nav-div">
       <header>
         <div className="nav-bar">
 
-          <div class="dropdown">
-            <button class="dropbtn">
+          <div className="dropdown">
+            <button className="dropbtn">
               Menu
             </button>
-            <div class="dropdown-content">
+            <div className="dropdown-content">
               <h2>
                 <a href="#about">
                   <span onClick={() => setCurrentPage("about")}>The Baker</span>
@@ -47,15 +47,65 @@ const Nav = (props) => {
               <Logo />
             </span>
           </a>
+
           <h2 className='login-nav'>
             <a href="#login">
-              <span onClick={() => setCurrentPage("login")}>Login/Logout</span>
+              <span onClick={() => Auth.logout()}>Logout</span>
             </a>
           </h2>
         </div>
       </header>
     </div>
-  );
+  );} else {
+    return (
+      <div className="nav-div">
+        <header>
+          <div className="nav-bar">
+  
+            <div className="dropdown">
+              <button className="dropbtn">
+                Menu
+              </button>
+              <div className="dropdown-content">
+                <h2>
+                  <a href="#about">
+                    <span onClick={() => setCurrentPage("about")}>The Baker</span>
+                  </a>
+                </h2>
+                <h2>
+                  <a href="#products">
+                    <span onClick={() => setCurrentPage("products")}>
+                      The Goodies
+                    </span>
+                  </a>
+                </h2>
+                <h2>
+                  <a href="#order">
+                    <span onClick={() => setCurrentPage("login")}>Order Now</span>
+                  </a>
+                </h2>
+                <h2>
+                  
+                </h2>
+              </div>
+            </div>
+            <a className='main-logo' href="#home">
+              <span onClick={() => setCurrentPage("home")}>
+                <Logo />
+              </span>
+            </a>
+  
+            <h2 className='login-nav'>
+              <a href="#login">
+                <span onClick={() => setCurrentPage("login")}>Login</span>
+              </a>
+            </h2>
+          </div>
+        </header>
+      </div>
+    )
+    
+  }
 };
 
 export default Nav;
